8c8f62413f0780c38fcedaf390b7ca1716d06043
Slava <loginmail2018@inbox.ru>
2022-11-29 22:59:46 +0300

    ***

--- src/main/java/web/dao/UserDAOImpl.java
+++ src/main/java/web/dao/UserDAOImpl.java
@@ -11,24 +11,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 @Repository
 public class UserDAOImpl implements UserDao{
-
-    private static final AtomicInteger AUTO_ID = new AtomicInteger(0);
-    private static Map<Integer, User> userMap = new HashMap<>();
-
-    static {
-        User us1 = new User("vania", "petrov");
-        us1.setId(1);
-        User us2 = new User("petya", "ivanov");
-        us2.setId(2);
-        User us3 = new User("masha", "fedotova");
-        us3.setId(3);
-        User us4 = new User("kira", "bushueva");
-        userMap.putIfAbsent(1, us1);
-        userMap.putIfAbsent(2, us2);
-        userMap.putIfAbsent(3, us3);
-        userMap.putIfAbsent(4, us4);
-    }
-
+    
 
     @Override
     public List<User> allUsers() {
